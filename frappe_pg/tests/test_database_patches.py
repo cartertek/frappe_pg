0cf6777a73c98dbd0a821214bb2e6e43d282efc7
chatgpt-a01 <chatgpt.a01.2428478@gmail.com>
2026-09-08 03:17:30 +0000

    fix: translate legacy mysql like string patterns

--- frappe_pg/tests/test_database_patches.py
+++ frappe_pg/tests/test_database_patches.py
@@ -239,6 +239,11 @@ class TestQueryTransformers(unittest.TestCase):
         query = 'SELECT * FROM "tabExample" WHERE "name" = "other_column"'
         self.assertEqual(convert_mysql_double_quoted_literals(query), query)
 
+    def test_double_quoted_mysql_like_pattern(self):
+        query = 'select data from "__UserSettings" where data like "%%%s%%"'
+        expected = "select data from \"__UserSettings\" where data like '%%%s%%'"
+        self.assertEqual(convert_mysql_double_quoted_literals(query), expected)
+
     def test_double_quoted_mysql_string_literals_in_legacy_in_list(self):
         query = (
             'SELECT * FROM "tabSingles" WHERE field in ('
