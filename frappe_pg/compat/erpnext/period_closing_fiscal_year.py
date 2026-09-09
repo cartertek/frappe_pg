9f1df1b7389141db771e9c3160bd6aa6088c21cb
chatgpt-a01 <chatgpt.a01.2428478@gmail.com>
2026-09-09 00:51:18 +0000

    refactor: register period closing compatibility

--- frappe_pg/compat/erpnext/period_closing_fiscal_year.py
+++ frappe_pg/compat/erpnext/period_closing_fiscal_year.py
@@ -79,13 +79,8 @@ def apply():
             return
 
         from frappe import _
-        from frappe.utils import formatdate
         from frappe import throw
-        throw(
-            _("Previous Fiscal Year {0} has not been closed. Please close it first.").format(
-                previous_fiscal_year[0]
-            )
-        )
+        throw(_("Previous Year is not closed, please close it first"))
 
     _patched = patched
     pcv.PeriodClosingVoucher.check_if_previous_year_closed = patched  # nosemgrep
